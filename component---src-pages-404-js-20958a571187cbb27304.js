1d79abe9b5f037464cd6d5a7573b91a6253a7317
sonmingi <smk692>
2023-04-17 23:54:35 +0900

    Updates

---
+++ component---src-pages-404-js-20958a571187cbb27304.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksmk692_github_io=self.webpackChunksmk692_github_io||[]).push([[883],{4393:function(e,t,n){n.r(t);var u=n(7294),l=n(4985),s=n(579);t.default=function(){return u.createElement(l.Z,null,u.createElement(s.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-20958a571187cbb27304.js.map
