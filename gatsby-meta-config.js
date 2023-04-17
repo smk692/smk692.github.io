dc6ab029e3e5fa6a75125c7d0b432d6d6dbc2616
손민기 <sonmingi@sonmingiui-MacBookPro-2.local>
2023-04-18 02:21:30 +0900

    feat: 댓글 기능 추가

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -6,7 +6,7 @@ module.exports = {
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
-      repo: ``, // `zoomkoding/zoomkoding-gatsby-blog`,
+      repo: `smk692/blog-comments`, // `zoomkoding/zoomkoding-gatsby-blog`,
     },
   },
   ga: '0', // Google Analytics Tracking ID
