ebeeda7ccd774518e8a3d27aa3b92d4f9a430c41
sonmingi <smk692>
2023-04-21 16:37:55 +0900

    feat: 자동사냥 테스트

---
+++ generate_blog_post.py
@@ -0,0 +1,42 @@
+import datetime
+import os
+
+def create_blog_post(emoji, title, tags, author,categories):
+    now = datetime.datetime.now()
+
+    # 블로그 포스트에 사용될 파일 이름
+    filename = "content/" + now.strftime("%Y-%m-%d") + "-" + title.lower().replace(" ", "-") + ".md"
+
+    # 블로그 포스트에 사용될 메타데이터
+    metadata = [
+        "---",
+        "layout: post",
+        f"emoji: {emoji}",
+        f"title: \"{title}\"",
+        f"date: {now}",
+        f"author: {author}",
+        f"tags: {tags}",
+        f"categories: {categories}",
+        "---"
+    ]
+
+    # 블로그 포스트에 사용될 본문
+    body = f"# {title}\n\n"
+
+    # 블로그 포스트 파일 생성
+    with open(filename, "w") as f:
+        f.write("\n".join(metadata))
+        f.write("\n")
+        f.write(body)
+
+    print(f"블로그 포스트 파일이 생성되었습니다: {filename}")
+
+
+if __name__ == "__main__":
+    emoji = input("🔮")
+    title = input("블로그 글 제목을 입력하세요: ")
+    tags = input("태그를 입력하세요 (쉼표로 구분): ").split(",")
+    author = input("손(Son/손민기)")
+    categories = input("카테고리를 입력하세요 (쉼표로 구분): ").split(",")
+
+    create_blog_post(emoji, title, tags, author, categories)
