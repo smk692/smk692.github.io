84b9509552c9bd27ac7158f3e6100b74682af601
손민기 <sonmingi@sonmingiui-MacBookPro-2.local>
2023-04-23 00:16:06 +0900

    feat 자동사냥을 위한 테스트

--- generate_blog_post.py
+++ generate_blog_post.py
@@ -30,14 +30,10 @@ def create_blog_post(emoji, title, tags, author, categories, contents):
         "---"
     ]
 
-    # 블로그 포스트에 사용될 본문
-    body = f"# {title}\n\n"
-
     # 블로그 포스트 파일 생성
     with open(file_name, "w") as f:
         f.write("\n".join(metadata))
         f.write("\n")
-        f.write(body)
         f.write(contents)
 
     print(f"블로그 포스트 파일이 생성되었습니다: {file_name}")
@@ -47,19 +43,20 @@ def generate_contents(topic):
 
     prompt_contents = f'''
         Please write in Korean:
-        From now on, you are an IT expert. Please write a blog post in markdown format on the topic of {topic}. Apply highlights, bolds, and italics to important words or sentences.
-        Define subheadings, advantages and disadvantages, application examples, and sample results in markdown format.
-        Provide clean and concise subheadings that match the content, and write an SEO-optimized post that is 3000 characters or less to ensure high visibility. Also, summarize the advantages and disadvantages of using the topic
-        The target audience for this post is expert developers, so make sure to make it readable and easy to understand.
-    '''
-    prompt_tags = f'''
-        {prompt_contents}
-        Could you please summarize and supplement the previous message into a message that is within 3300 characters? The previous message contained information regarding the previous conversation and it would be great if it could be made to sound more human-like and create a place to insert images.
-        Impontant! Please supplement and continue writing based on the initial question and extract 30 hashtags relevant to the content and list them at the beginning of the post using a comma as a separator.
+        
+        As an IT expert, let's create a blog post in markdown format on the topic of {topic}.
+        Oh, and please remove the title.
+        
+        Please apply highlights, bolds, and italics to important words or sentences.
+        Define subheadings, advantages and disadvantages, application examples, Java sample code, and code results in markdown format.
+        Provide clean and concise subheadings that match the content, and write an SEO-optimized post.
+        Please summarize the pros and cons of using the topic as the target audience for this post is expert developers, so make it readable and easy to understand.
+        It would be great to make the post sound more human-like and create a space to insert images by including information about the previous conversation.
+
+        Important! Please supplement and continue writing based on the initial question, and extract 30 hashtags relevant to the content and list them at the beginning of the post using a comma as a separator.
     '''
 
     contents = connection_chatgpt(prompt_contents)
-    contents += connection_chatgpt(prompt_tags)
 
     return contents, hashtag_export(contents)
 
@@ -68,7 +65,7 @@ def connection_chatgpt(prompt):
         model="text-davinci-003",
         prompt=prompt,
         temperature=0.1,
-        max_tokens=3500,
+        max_tokens=3800,
         top_p=1,
         frequency_penalty=0.0,
         presence_penalty=0.6,
@@ -99,7 +96,7 @@ def create_emoji():
     return chr(random.randint(start, end))
 
 if __name__ == "__main__":
-    topic = "Kafka 구조, 내용 및 사용 사례"
+    topic = "Kafka AWS MSK 내용 및 사용 사례"
     contents, tags = generate_contents(topic)
 
     pprint.pprint("-----------------------")
