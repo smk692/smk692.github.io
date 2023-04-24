a36a522840b01da7780915c92848f519f4b400ef
smk692 <smk692@naver.com>
2023-04-24 15:43:21 +0900

    feat: 자동사냥 테스트

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -21,7 +21,8 @@ function BlogTemplate({ data }) {
     if (!siteUrl) return;
     const namespace = siteUrl.replace(/(^\w+:|^)\/\//, '');
     const key = curPost.slug.replace(/\//g, '');
-
+    console.log(namespace)
+    console.log(key)
     fetch(
       `https://api.countapi.xyz/${
         process.env.NODE_ENV === 'development' ? 'get' : 'hit'
